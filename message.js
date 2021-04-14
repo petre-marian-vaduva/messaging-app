21cd26410839beaecb8ac8f192a69ee3ca62844f
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-04-15 00:01:01 +0100

    create new li element

--- message.js
+++ message.js
@@ -3,5 +3,7 @@ const textbox = document.querySelector("#textbox");
 const button = document.querySelector("#button");
 
 button.addEventListener('click', () => {
-    
+    let list = document.createElement('li');
+    list.innerHTML = textbox.value;
+    messages.appendChild(list)
 })
